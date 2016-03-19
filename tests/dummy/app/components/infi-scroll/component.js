3a3c82d1b97cbffbd82eddfca7da71916c1a986c
Conor Linehan <conorlinehan9@gmail.com>
2016-03-19 14:03:21 +0000

    crude implementation of visible items

--- tests/dummy/app/components/infi-scroll/component.js
+++ tests/dummy/app/components/infi-scroll/component.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 import layout from './template';
 
+/*
+The purpose of this component is to act as an infi scroll generator
+ */
 export default Ember.Component.extend({
   layout: layout,
 
@@ -10,25 +13,45 @@ export default Ember.Component.extend({
   containerHeight: 600,
   scrollLeft: 0,
   scrollTop: 200,
-  items: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
 
-  setScroll(x, y) {
-    this.set('scrollLeft', x);
-    this.set('scrollTop', y);
+  init() {
+    this._super(...arguments);
+    this.set('items', this._genRange(-20, 20));
+    const totalHeight = this.get('items.length') * this.get('itemHeight');
+    this.set('totalHeight', totalHeight);
+  },
+
+  _genRange(start, end) {
+    let result = [];
+    var n = start;
+    while (n < end) {
+      result.push(n);
+      n += 1;
+    }
+    return result;
+  },
+
+
+  /*
+  @method returns the visible items
+   */
+  visibleItems(topPoint) {
+    const topIndex = this._pointToIndex(topPoint);
+    console.log(`Top Index: ${topIndex}`);
+    const lastIndex = topIndex + (this.get('containerHeight') / this.get('itemHeight'));
+    return this.get('items').slice(topIndex, lastIndex);
+  },
+
+  _pointToIndex(point) {
+    return parseInt(point / this.get('itemHeight'));
   },
 
   actions: {
-    scrollChange: function(scrollLeft, scrollTop){
-      let currentX = scrollLeft;
-      let currentY = scrollTop;
-      const contentHeight = this.get('containerHeight');
-      if ((currentY) < (contentHeight / 8.0)) {
-        this.setScroll(currentX,(currentY + (contentHeight / 2.0)));
-      }
-
-      if ((currentY) > ((contentHeight * 6) / 8.0)) {
-        this.setScroll(currentX, (currentY - (contentHeight / 2.0)));
-      }
+    scrollChange: function(_, scrollTop){
+      this.set('scrollTop', scrollTop);
+      console.log(`Scroll Top: ${scrollTop}`);
+      const visibleItems = this.visibleItems(scrollTop);
+      console.log(visibleItems);
     }
   }
 });
